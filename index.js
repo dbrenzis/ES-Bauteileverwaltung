77786aa97164faf0c7ced1842ad1ca7596ee4f1d
dbrenzis <daniel_dominik.brenzis@smail.th-koeln.de>
2019-11-26 09:18:42 +0100

    Bauteile VIews, Ausleihauftrag

--- index.js
+++ index.js
@@ -11,6 +11,23 @@ app.get("/", (req, res) => {
   res.render("index", { namen: "Daniel Brenzis", userType: "administrator" });
 });
 
+app.get("/bauteile/bearbeiten", (req, res) => {
+  res.render("bauteilUpdate", { namen: "Daniel Brenzis", userType: "administrator",
+    bauteil: {
+      name: "Bauteil1",
+      beschreibung:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto at, quas blanditiis, ipsa animi libero laboriosam vitae saepe sit et rerum cupiditate repellat odio quidem magni ratione nihil delectus adipisci quos enim minima obcaecati aliquam? Blanditiis nulla corrupti, placeat iste, voluptatum cum laborum harum aliquam voluptas cupiditate, nihil totam fuga.",
+      ortsangabe: "Schrank15, linkes Fach 14a 13b",
+      anzahl: 15,
+      status: "aktiv",
+      artikelnummer: "1234512-12345da2"
+    }});
+});
+
+app.get("/testlogin", (req, res) => {
+  res.render("index_login");
+});
+
 app.get("/bauteile", (req, res) => {
   const user = {
     namen: "Daniel Brenzis",
@@ -107,6 +124,24 @@ app.get("/bauteile", (req, res) => {
 
 app.get("/bauteile/:id", (req, res) => {
   res.render("bauteil", {
+    namen: "Daniel Brenzis",
+    userType: "administrator",
+    anzahlausgeliehen: 12,
+    bauteil: {
+      name: "Bauteil1",
+      beschreibung:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto at, quas blanditiis, ipsa animi libero laboriosam vitae saepe sit et rerum cupiditate repellat odio quidem magni ratione nihil delectus adipisci quos enim minima obcaecati aliquam? Blanditiis nulla corrupti, placeat iste, voluptatum cum laborum harum aliquam voluptas cupiditate, nihil totam fuga.",
+      ortsangabe: "Schrank15, linkes Fach 14a 13b",
+      anzahl: 15,
+      status: "aktiv",
+      artikelnummer: "1234512-12345da2"
+    }
+  });
+});
+
+
+app.get("/ausleihauftraege", (req, res) => {
+  res.render("ausleihauftraegeList", {
     namen: "Daniel Brenzis",
     userType: "administrator",
     anzahlausgeliehen: 12,
